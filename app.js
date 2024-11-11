61e50a27058e4ea2d4143cf753e2ad16141ff6b5
Kevon <seni0026@algonquinlive.com>
2024-11-10 23:08:34 -0500

    use math calculations

--- app.js
+++ app.js
@@ -30,3 +30,27 @@ totalPrice = 4
 
 //calling the function calculateShippingCost and passing totalPrice as the parameter
 calculateShippingCost(totalPrice)
+
+// when calculating money, do the calculation in cents then divide by 100 to convert to
+// function to round numbers
+Math.round()
+
+let r = 4;
+let s = 5;
+let m = r + s;
+
+// alert('The sum is ' + m)
+
+let cartQuantity = 0;
+
+const name = 'Kevon';
+console.log(`My name is ${name}`);
+
+const cost = 5 + (3 * 2) + 9;
+console.log(`Cost of food: $${cost}`);
+
+const tax = cost * 0.1;
+console.log(`Tax (10%): $${tax}`);
+
+const totalCost = cost + tax;
+console.log(`Total cost: $${totalCost}`);
